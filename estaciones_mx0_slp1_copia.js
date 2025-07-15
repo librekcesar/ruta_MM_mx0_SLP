e7013e66f8e10fd4103d8e5a996d30d0a60a21bd
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-15 15:08:52 -0600

    Add files via upload

---
+++ estaciones_mx0_slp1_copia.js
@@ -0,0 +1,9 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones_mx0_slp1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "19.64220097106514, -99.14250999381147", "description": "MX0", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -99.1422607, 19.6422227, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "22.13720977627198, -100.96040075767105", "description": "SLP1", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -100.960413, 22.137222, 0.0 ] } }
+]
+}
---
+++ paradas_mx0_slp1_copia.js
@@ -0,0 +1,9 @@
+var seguras = {
+"type": "FeatureCollection",
+"name": "paradas_mx0_slp1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Pemex - 12315", "description": "Nochebuena, 76820 Loma Linda, Qro.", "layer": "PEMEX_MX0_SLP1", "direccion": "Nochebuena, 76820 Loma Linda, Qro." }, "geometry": { "type": "Point", "coordinates": [ -100.0343716, 20.4068235, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Pemex", "description": "San Luis Potosí - Santiago de Querétaro 820, Barrio de San Juan, 79560 Santa María del Río, S.L.P.", "layer": "PEMEX_MX0_SLP1", "direccion": "San Luis Potosí - Santiago de Querétaro 820, Barrio de San Juan, 79560 Santa María del Río, S.L.P." }, "geometry": { "type": "Point", "coordinates": [ -100.7300801, 21.807956, 0.0 ] } }
+]
+}
